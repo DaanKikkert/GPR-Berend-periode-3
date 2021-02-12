3a9a5171ae057ec91ad292903f4f88ea3c27c2e8
Daan Kikkert <54845837+DaanKikkert@users.noreply.github.com>
2021-02-12 12:09:16 +0100

    Y

---
+++ GPR Periode 3 B/Assets/Scripts/HealthUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] Text healthText;
+    [SerializeField] PlayerHealth playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = gameObject.GetComponent<Text>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = playerHealth.getHealth().ToString();
+    }
+
+    public void UpdateUI(float health)
+    {
+        healthText.text = health.ToString();
+    }
+}
---
+++ GPR Periode 3 B/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    float currentHealth;
+    [SerializeField] private float maxHealth = 100;
+    
+    private void Start()
+    {
+        currentHealth = maxHealth;        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int dmgAmount)
+    {
+        currentHealth -= dmgAmount;        
+        if(currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    public float getHealth()
+    {
+        return currentHealth;
+    }
+}
